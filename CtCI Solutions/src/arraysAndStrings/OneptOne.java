c90305aab1ef447b67e075f2d7caeeec64315729
Kevin Li <kevli2231@gmail.com>
2015-10-03 15:28:36 -0500

    Finished problem 1.1 and created a program that checks for pangrams.

--- CtCI Solutions/src/arraysAndStrings/OneptOne.java
+++ CtCI Solutions/src/arraysAndStrings/OneptOne.java
@@ -9,7 +9,31 @@ import java.util.*;
  */
 public class OneptOne {
 	public static void main(String [] args){
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Please enter a sentence: ");
+		String sentence = reader.nextLine();
+		if(isUniqueChars(sentence)){
+			System.out.println("The sentence cotains all unique characters.");
+		}
+		else {
+			System.out.println("This sentence contains duplicate characters.");
+		}
 		
+	}
+	
+	public static boolean isUniqueChars(String sentence){
+		boolean isUnique = true;
+		Set<Character> uniqueChar = new HashSet<Character>();
+		for(int i = 0; i < sentence.length(); i++){
+			if(uniqueChar.contains(sentence.charAt(i))){
+				isUnique = false;
+				i = sentence.length();
+			}
+			else {
+				uniqueChar.add(sentence.charAt(i));
+			}
+		}
+		return isUnique;
 	}
 
 }
---
+++ CtCI Solutions/src/arraysAndStrings/Pangrams.java
@@ -0,0 +1,34 @@
+package arraysAndStrings;
+import java.util.*;
+public class Pangrams {
+	public static void main(String [] args){
+		Set<Character> alphabet = new HashSet<>();
+		
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Please enter a string of text:");
+		String sentence = reader.nextLine();
+		
+		for(char ch = 'a'; ch <= 'z'; ch++){
+			alphabet.add(ch);
+		}
+		
+		sentence.toLowerCase();
+		for(int i = 0; i < sentence.length(); i++){
+			if(alphabet.contains(sentence.charAt(i))){
+				alphabet.remove(sentence.charAt(i));
+			}
+		}
+		
+		if(alphabet.isEmpty()){
+			System.out.println("Sentence is a pangram!");
+		}
+		else {
+			System.out.println("The sentence is missing: ");
+			for(char c : alphabet){
+				System.out.print(c);
+			}
+		}
+	}
+	
+	
+}
