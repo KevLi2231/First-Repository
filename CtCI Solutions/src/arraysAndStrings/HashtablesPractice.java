60e00ccad1f3b014db6e1d477ac36d1163165fc7
Kevin Li <kevli2231@gmail.com>
2015-10-03 14:20:41 -0500

    Practice Using Git

---
+++ CtCI Solutions/src/arraysAndStrings/HashtablesPractice.java
@@ -0,0 +1,8 @@
+package arraysAndStrings;
+import java.util.*;
+
+public class HashtablesPractice {
+	public static void main(String [] args){
+		
+	}
+}
---
+++ CtCI Solutions/src/arraysAndStrings/OneptOne.java
@@ -0,0 +1,15 @@
+package arraysAndStrings;
+import java.util.*;
+
+/**
+ * Implement an algorithm to determine if a string has all unique characters. 
+ * What if you cannot use additional data structures?
+ * @author li223
+ *
+ */
+public class OneptOne {
+	public static void main(String [] args){
+		
+	}
+
+}
